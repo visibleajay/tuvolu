6dc256d1f545ec31ea9d62af1942ee0c0c088e8f
Ajay Kumar <visibleajay@gmail.com>
2018-01-02 21:10:43 +0530

    add tuvolu state and actions.

--- src/tuvolu/core/tuvolu.actions.ts
+++ src/tuvolu/core/tuvolu.actions.ts
@@ -1,14 +1,80 @@
 
+import { Injectable } from '@angular/core';
 
+import { Action } from 'redux';
+import { NgRedux } from '@angular-redux/store';
+import { ITuvoluState } from './tuvolu.state';
+import { HttpService } from '../service/http.service';
+
+@Injectable()
 export class TuvoluActions {
 
+    static ADD_SEARCH_DATA = "ADD_SEARCH_DATA"
     static SEARCH_URL = "SEARCH_URL";
-    static SHOW_LOADING_STATE = 'SHOW_LOADING_STATE';
+    static UPDATE_LOADING_STATE = 'UPDATE_LOADING_STATE';
     static ADD_TVDATA = "ADD_TVDATA";
-    static SHOW_TVDATA = 'SHOW_TVDATA';
 
-    constructor() {
+    constructor(private ngRedux: NgRedux<ITuvoluState>,
+                private http: HttpService) {
     }
 
+    getData(value: String) {
+        this.ngRedux.dispatch(
+            this.addSearchString(value)
+        );
+        this.ngRedux.dispatch(this.updateLoadingData(true));
+        this.makeAPIRequest(value).subscribe(data => this.updateData(data));
+    }
 
+    updateData(data) {
+        this.ngRedux.dispatch(
+            this.addTVData(data)
+        )
+        this.ngRedux.dispatch(
+            this.updateLoadingData(false)
+        )
+    }
+
+    updateLoadingData(value: Boolean) {
+        return {
+            type: TuvoluActions.UPDATE_LOADING_STATE,
+            payload: {
+                loading: value
+            }
+        }
+    }
+
+    addTVData(data) {
+        return {
+            type: TuvoluActions.ADD_TVDATA,
+            payload: {
+                data: this.getTVData(data)
+            }
+        }
+    }
+
+    getTVData(tvDataSet) {
+        return tvDataSet.map((info) => {
+            return {
+                image: info['show']['image'] ? info['show']['image']['medium'] : 'NA',
+                link: info['show']['name'].link(info['show']['url']).replace(/^<a/, '$& target="_blank"') || 'NA',
+                rating: info['show']['rating'] ? info['show']['rating']['average'] ? info['show']['rating']['average'] : 0  : 0,
+                genres: info['show']['genres'],
+                summary: info['show']['summary'] || 'Not Available',        
+            }
+        });
+    }
+
+    addSearchString(searchValue: String) {
+        return {
+            type: TuvoluActions.ADD_SEARCH_DATA,
+            payload: {
+                data: searchValue
+            }
+        }
+    }
+
+    makeAPIRequest(value: String) {
+        return this.http.getData(value);
+    }
 }
--- src/tuvolu/core/tuvolu.state.ts
+++ src/tuvolu/core/tuvolu.state.ts
@@ -1,4 +1,6 @@
 
+import { TuvoluActions } from './tuvolu.actions';
+
 export interface IDataState {
     imageUrl: String;
     infoUrl: String;
@@ -13,3 +15,31 @@ export interface ITuvoluState{
     data: IDataState[];
 }
 
+export const INITIAL_STATE = {
+    searchText: '',
+    loading: false,
+    data: []
+}
+
+export function rootReducer(lastState = INITIAL_STATE, action) {
+    switch(action.type) {
+        case TuvoluActions.ADD_SEARCH_DATA:
+            return Object.assign({},
+                lastState,
+                {searchText: action.payload.data}
+            );
+        case TuvoluActions.UPDATE_LOADING_STATE:
+            return Object.assign({},
+                lastState,
+                {loading: action.payload.loading}    
+            );
+        case TuvoluActions.ADD_TVDATA:
+            return Object.assign(
+                {},
+                lastState,
+                {data: action.payload.data}
+            ) ;
+        default:
+            lastState;
+    }
+}
