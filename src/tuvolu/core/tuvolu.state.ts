7073ccd8f152952b9298862bb08cd5272b5105d6
Ajay Kumar <visibleajay@gmail.com>
2018-01-04 10:54:32 +0530

    use es6 object spread to create new objects.

--- src/tuvolu/core/tuvolu.state.ts
+++ src/tuvolu/core/tuvolu.state.ts
@@ -24,22 +24,21 @@ export const INITIAL_STATE = {
 export function rootReducer(lastState = INITIAL_STATE, action) {
     switch(action.type) {
         case TuvoluActions.ADD_SEARCH_DATA:
-            return Object.assign({},
-                lastState,
-                {searchText: action.payload.data}
-            );
+            return {
+                ...lastState,
+                searchText: action.payload.data
+            };
         case TuvoluActions.UPDATE_LOADING_STATE:
-            return Object.assign({},
-                lastState,
-                {loading: action.payload.loading}    
-            );
+            return {
+                ...lastState,
+                loading: action.payload.loading
+            };
         case TuvoluActions.ADD_TVDATA:
-            return Object.assign(
-                {},
-                lastState,
-                {data: action.payload.data,
-                loading: action.payload.loading}
-            );
+            return {
+                ...lastState,
+                data : action.payload.data,
+                loading : action.payload.loading
+            };
         case TuvoluActions.GET_REQUEST_ERROR:
             return lastState;
         default:
